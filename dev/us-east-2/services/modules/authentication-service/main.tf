296fd9a7a828009fd35600afc888be225fe9d59a
Lucas Viana <lucas.viana@tmaior.com.br>
2024-02-22 13:46:15 -0300

    feat: UIUrl monolith updated

--- dev/us-east-2/services/modules/authentication-service/main.tf
+++ dev/us-east-2/services/modules/authentication-service/main.tf
@@ -20,7 +20,7 @@ locals {
   setting_json = jsonencode(<<EOT
     {
       "ConnectionStrings": {
-        "VMDB": 'Data Source=sql-server-backup-development.sql-server-backup.svc.cluster.local;Initial Catalog=UAT_ENT_VMDB;Persist Security Info=True;User ID=sa;Password=password123@;Encrypt=false'
+        "VMDB": "Data Source=sql-server-backup-development.sql-server-backup.svc.cluster.local,1433;Initial Catalog=UAT_ENT_VMDB;Persist Security Info=True;User ID=sa;Password=password123@;Encrypt=false"
       },
       "Logging": {
         "LogLevel": {
