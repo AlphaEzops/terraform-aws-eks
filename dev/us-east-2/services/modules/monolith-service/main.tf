e60326aaaf5d7298a5fa714dcdcedea2c6af73d9
Lucas Viana <lucas.viana@tmaior.com.br>
2024-02-26 20:04:56 -0300

    fix: modules

--- dev/us-east-2/services/modules/monolith-service/main.tf
+++ dev/us-east-2/services/modules/monolith-service/main.tf
@@ -17,6 +17,17 @@ locals {
   service_account_name = var.service_account_name
 }
 
+locals {
+  DB_HOST = jsondecode(data.aws_secretsmanager_secret_version.secret_reveal.secret_string)["DB_HOST"]
+  DB_PORT = jsondecode(data.aws_secretsmanager_secret_version.secret_reveal.secret_string)["DB_PORT"]
+  DB_VMDB_NAME = jsondecode(data.aws_secretsmanager_secret_version.secret_reveal.secret_string)["DB_VMDB_NAME"]
+  DB_VEDB_NAME = jsondecode(data.aws_secretsmanager_secret_version.secret_reveal.secret_string)["DB_VEDB_NAME"]
+  DB_VSDB_NAME = jsondecode(data.aws_secretsmanager_secret_version.secret_reveal.secret_string)["DB_VSDB_NAME"]
+  DB_USERNAME = jsondecode(data.aws_secretsmanager_secret_version.secret_reveal.secret_string)["DB_USERNAME"]
+  DB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.secret_reveal.secret_string)["DB_PASSWORD"]
+}
+
+
 # module "custom_external_secret_monolith_service" {
 #   source = "../../system/external-secrets-role"
 #   application_namespace = local.application_namespace
@@ -50,6 +61,21 @@ spec:
     helm:
       valueFiles:
         - values.yaml
+      parameters:
+        - name: "configMap.DefaultConnectionString"
+          value: "data source=${local.DB_HOST},${local.DB_PORT};initial catalog=${local.DB_VEDB_NAME};user id=${local.DB_USERNAME};pwd=${local.DB_PASSWORD};MultipleActiveResultSets=True;Enlist=false;App=EntityFramework"
+        - name: "configMap.CaseCustodianDataSourceContextConnectionString" 
+          value: "data source=${local.DB_HOST},${local.DB_PORT};initial catalog=${local.DB_VEDB_NAME};user id=${local.DB_USERNAME};pwd=${local.DB_PASSWORD};multipleactiveresultsets=True;application name=EntityFramework"
+        - name: "configMap.MasterEntitiesConnectionString"
+          value: "metadata=res://*/Master.Master.csdl|res://*/Master.Master.ssdl|res://*/Master.Master.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=${local.DB_HOST},${local.DB_PORT};initial catalog=${local.DB_VMDB_NAME};user id=${local.DB_USERNAME};pwd=${local.DB_PASSWORD};MultipleActiveResultSets=True;Enlist=false;App=EntityFramework&quot;"
+        - name: "configMap.ProcessingEngineEntitiesConnectionString"
+          value: "metadata=res://*/ProcessingEngineManagement.ProcessingEngine.csdl|res://*/ProcessingEngineManagement.ProcessingEngine.ssdl|res://*/ProcessingEngineManagement.ProcessingEngine.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=source=${local.DB_HOST},${local.DB_PORT};initial catalog=${local.DB_VMDB_NAME};user id=${local.DB_USERNAME};pwd=${local.DB_PASSWORD};MultipleActiveResultSets=True;Enlist=false;App=EntityFramework&quot;"
+        - name: "configMap.ReportManagementEntitiesConnectionString"
+          value: "metadata=res://*/ReportManagement.ReportManagement.csdl|res://*/ReportManagement.ReportManagement.ssdl|res://*/ReportManagement.ReportManagement.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=${local.DB_HOST},${local.DB_PORT};initial catalog=${local.DB_VMDB_NAME};user id=${local.DB_USERNAME};pwd=${local.DB_PASSWORD};MultipleActiveResultSets=True;Enlist=false;App=EntityFramework&quot;"
+        - name: "configMap.EntityDbConnectionString"
+          value: "metadata={0};provider=System.Data.SqlClient;provider connection string=&quot;data source={1};initial catalog={2};{3};MultipleActiveResultSets=True;Enlist={4};App=EntityFramework&quot;"
+        - name: "configMap.StagingEntitiesConnectionString"
+          value: "metadata=res://*/Staging.Staging.csdl|res://*/Staging.Staging.ssdl|res://*/Staging.Staging.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=source=${local.DB_HOST},${local.DB_PORT};initial catalog=${local.DB_VSDB_NAME};user id=${local.DB_USERNAME};pwd=${local.DB_PASSWORD};MultipleActiveResultSets=True;App=EntityFramework&quot;"
     path: dev/us-east-2/services/apps/monolith-service
     repoURL: 'git@github.com:AlphaEzops/reveal-eks.git'
     targetRevision: HEAD
